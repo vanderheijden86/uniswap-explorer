ff4aa54fe450a9729aa270e010ca47cf2801dd92
vanderheijden86 <vanderheijden86@gmail.com>
2022-07-13 13:51:59 +0200

    feat: insert a number of hooks from web3 boilerplate project

---
+++ src/hooks/useContract.ts
@@ -0,0 +1,29 @@
+import { Contract } from "@ethersproject/contracts";
+import { useWeb3React } from "@web3-react/core";
+import { useMemo } from "react";
+
+/**
+ *
+ * @param address the address of the contract to load
+ * @param ABI the ABI of the contract
+ */
+export default function useContract<T extends Contract = Contract>(
+  address: string,
+  ABI: any
+): T | null {
+  const { library, account, chainId } = useWeb3React();
+
+  return useMemo(() => {
+    if (!address || !ABI || !library || !chainId) {
+      return null;
+    }
+
+    try {
+      return new Contract(address, ABI, library.getSigner(account));
+    } catch (error) {
+      console.error("Failed To Get Contract", error);
+
+      return null;
+    }
+  }, [address, ABI, library, account]) as T;
+}
